d41232a07e27577244513739306848d0de888f32
AuroreMariscal <aurore@affinitic.be>
2016-10-26 13:22:43 +0200

    Add test-all parts
    This refs affinitic #8959

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ setup(name='affinitic.simplecookiecuttr',
       version=version,
       description="Basic integration of jquery.cookiecuttr.js for Plone 3",
       long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
+          "Framework :: Plone",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
       keywords='Affinitic',
       author='Affinitic',
       author_email='info@affinitic.be',
@@ -30,4 +30,7 @@ setup(name='affinitic.simplecookiecuttr',
           'collective.js.jquery',
           'plone.memoize',
           # -*- Extra requirements: -*-
-      ])
+      ],
+      extras_require=dict(
+          test=[])
+      )
